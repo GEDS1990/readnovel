41e54b8d9203ac8a715f5b20d1202a2bb5e7cce6
ge-dsh <ge-dsh@neusoft.com>
2020-11-25 18:07:45 +0800

    mod

--- light-reading-cloud/docker-compose/db/init/reading_cloud_account.sql
+++ light-reading-cloud/docker-compose/db/init/reading_cloud_account.sql
@@ -15,6 +15,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+-- 创建数据库:reading_cloud_account
+use mysql;
+create database reading_cloud_account;
 --
 -- Table structure for table `user`
 --
--- light-reading-cloud/docker-compose/db/init/reading_cloud_resource.sql
+++ light-reading-cloud/docker-compose/db/init/reading_cloud_resource.sql
@@ -15,6 +15,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+-- 创建数据库:reading_cloud_resource
+use mysql;
+create database reading_cloud_resource;
 --
 -- Table structure for table `index_page_config`
 --
